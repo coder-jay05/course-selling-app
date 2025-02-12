470bcb6e20836643545cca95e1d79cbda1d3cc1c
Jayprakash Mahato <114814784+coder-jay05@users.noreply.github.com>
2025-02-12 21:16:42 +0530

    url changed new

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(fileUpload({
 
 app.use(
     cors({
-         origin: process.env.FRONTEND_URL,
+         origin: process.env.FRONTEND_URL?.trim(),
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
